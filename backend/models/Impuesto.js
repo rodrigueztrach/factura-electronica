4c1690f1f8a59174fe67fed373b6eb648d61a0b5
rodrigueztrach <rronny045@gmail.com>
2026-01-16 14:37:20 -0600

    se cambia la estructura de impuesto

---
+++ backend/models/Impuesto.js
@@ -0,0 +1,28 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/db.js";
+
+const Impuesto = sequelize.define("Impuesto", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  nombre_impuesto: {
+    type: DataTypes.STRING(50),
+    allowNull: false
+  },
+  porcentaje: {
+    type: DataTypes.DECIMAL(5, 2),
+    allowNull: false
+  },
+  activo: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: true
+  }
+}, {
+  tableName: "impuestos",
+  timestamps: false
+});
+
+export default Impuesto;
