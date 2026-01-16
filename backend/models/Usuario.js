db65a39d16967194d483c69c6af06d28ef9396e0
rodrigueztrach <rronny045@gmail.com>
2026-01-16 14:27:32 -0600

    se crea el usuario

---
+++ backend/models/Usuario.js
@@ -0,0 +1,41 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/db.js";  
+
+const Usuario = sequelize.define("Usuario", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  nombre: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true
+    }
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  rol: {
+    type: DataTypes.ENUM("admin", "usuario"), // puedes agregar más roles si quieres
+    allowNull: false,
+    defaultValue: "usuario"
+  },
+  fecha_creacion: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW
+  }
+}, {
+  tableName: "usuarios",
+  timestamps: false
+});
+
+export default Usuario;
